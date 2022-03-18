2e933d37ab7bd55b6fa0a5da79433e79a992e423
Sharukh <sharukhsm@gmail.com>
2022-03-18 18:27:56 +0530

    useContext complete

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@ import React from "react";
 import { ComponentC } from "./components/ComponentC";
 
 export const UserContext = React.createContext();
+export const ChannelContext = React.createContext();
+
 function App() {
   return (
     <div className="App">
       <UserContext.Provider value={"sharukh"}>
-        <ComponentC />
+        <ChannelContext.Provider value={"Alpha"}>
+          <ComponentC />
+        </ChannelContext.Provider>
       </UserContext.Provider>
     </div>
   );
